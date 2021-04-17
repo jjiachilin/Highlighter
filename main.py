a0d7737158eb4df63915f6bb808e2c62aef8a6e6
lukededau <lukededau@gmail.com>
2021-04-17 12:08:12 -0700

    implemented user record timestamp on key press and then save them into a file

---
+++ main.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import keyboard
+import os
+
+def record_ts(fh):
+    time_now = datetime.now().time().strftime('%H:%M:%S.%f')
+    print("Timestamp recorded: " + str(time_now[:-7]))
+    # trim off the microseconds
+    fh.write(time_now[:-7] + "\n")
+
+def main():
+    print("Hightlighter has started...")
+    print("Press 'ctrl+shift+r' to record the timestamp")
+    print("Press 'ctrl+shift+q' to exit program")
+
+    # checks whether the directory exists
+    if not os.path.exists('ts_recordings'):
+        os.makedirs('ts_recordings')
+
+    # get program start time for file name
+    start_time = datetime.now()
+    formatted_time = start_time.strftime('%Y-%m-%d_%I-%M-%S%p')
+
+    # get path to write files to ts_recordings directory
+    cur_path = os.path.dirname(__file__)
+    rel_path = 'ts_recordings\\ts-recording_%s.txt' % formatted_time
+    new_path = os.path.join(cur_path, rel_path)
+
+    fh = open(new_path, 'w')
+
+    # you may change the keystoke commands as you see fit
+    keyboard.add_hotkey('ctrl+shift+r', record_ts, args=[fh])
+    keyboard.wait('ctrl+shift+q')
+    print("Exiting Highlighter...")
+
+    fh.close()
+
+
+if __name__ == '__main__':
+    main()
