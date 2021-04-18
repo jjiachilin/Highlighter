e0c976d584f445da9a466c55d9ca9fff6343fb7f
hubert322 <huberth@umich.edu>
2021-04-18 13:15:11 +0800

    made summarize driver program. need to import necessary files

--- parse.py
+++ parse.py
@@ -2,15 +2,21 @@ import re
 import json
 import demjson
 
-with open("transcript.html") as f:
-    transcript = f.read()
-    print(len(transcript))
 
-pattern = "window.__data__.transcriptList.push\( (.+?)\);"
+def get_transcript_json(transcript_path):
+    with open(transcript_path) as f:
+        transcript = f.read()
 
-results = re.findall(pattern, transcript, re.DOTALL)
-for i, result in enumerate(results):
-    results[i] = demjson.decode(result)
+    pattern = "window.__data__.transcriptList.push\( (.+?)\);"
 
-with open("transcript.json", "w") as f:
-    json.dump(results, f, indent=4)
+    results = re.findall(pattern, transcript, re.DOTALL)
+    for i, result in enumerate(results):
+        results[i] = demjson.decode(result)
+
+    with open("transcript.json", "w") as f:
+        json.dump(results, f, indent=4)
+
+    return results
+
+if __name__ == "__main__":
+    get_transcript_json("transcript.html")
