2b0ee597369301b8ef2098bc56a8b14a0bdda679
Joseph <joseph.lin@ubiik.com>
2021-04-18 13:22:06 +0800

    keywords

--- summary.py
+++ summary.py
@@ -4,6 +4,7 @@ import json
 
 openai.api_key = "sk-7feZk6oBEYLBfhLXSyF9wFjUrnBlwgt4J96Gqctp"
 
+"""
 f = open("lecture_subs.txt")
 
 all_text = f.read().replace("\n", " ")
@@ -11,9 +12,7 @@ all_text = f.read().replace("\n", " ")
 f.close()
 
 f = open("lecture_subs.txt")
-
-"""
-selected_text = "What is the key point in this text?\n'''\n"
+selected_text = ""
 
 for _ in range(176):
     f.readline()
@@ -23,23 +22,47 @@ for _ in range(88):
     if new_line == "\n":
         continue
     selected_text += new_line.replace("\n", " ")
-
-selected_text += "\n'''\n The key point is:\n"
 """
 
-f = open("selected.txt")
-selected_text = f.read().replace("\n", " ")
+# returns string summary
+def get_summary(selected_text):
+    selected_text.replace("\n", " ")
+    selected_text += "\n\ntl;dr"
 
-print(selected_text)
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=selected_text,
+        temperature=0.2,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
 
-response = openai.Completion.create(
-  engine="davinci",
-  prompt=selected_text,
-  temperature=0.1,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+    # clean up response
+    response = response["choices"][0]["text"]
+    if response[len(response)-1] != ".":
+        if response.rfind(".") != -1:
+            response = response[:response.rfind(".")+1]
+    response = response.replace("\n", " ")    
+    while not response[0].isalnum():
+        response = response[1:]
+    return response.strip()
 
-print(response)
+# returns an array of strings for keywords
+def get_keywords(selected_text):
+    selected_text = "Text: " + selected_text + " \n\nKeywords:"
+    keywords = openai.Completion.create(
+        engine="davinci",
+        prompt=selected_text,
+        temperature=0.3,
+        max_tokens=max(20, int(len(selected_text)/15)),
+        top_p=1.0,
+        frequency_penalty=0.9,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    return keywords["choices"][0]["text"].replace(" ", "").split(",")
+
+print(get_summary(selected_text))
+print(get_keywords(selected_text))
